2328d1887bebfa19215a266a439f2c670b386461
ps229592 <93653466+ps229592@users.noreply.github.com>
2023-05-29 21:09:01 +0200

    ja

--- thetrailers/lib/pages/movies/movies_trailers.dart
+++ thetrailers/lib/pages/movies/movies_trailers.dart
@@ -3,6 +3,7 @@ import 'package:thetrailers/models/trailer.dart';
 import 'package:thetrailers/services/movie_service.dart';
 // import 'package:thetrailers/services/trailer_service.dart';
 import 'package:flutter/material.dart';
+import 'package:thetrailers/services/trailer_service.dart';
 
 class MoviesTrailersPage extends StatefulWidget {
   MoviesTrailersPage({Key? key, required this.movie}) : super(key: key);
@@ -14,11 +15,47 @@ class MoviesTrailersPage extends StatefulWidget {
 }
 
 class _MoviesTrailersPageState extends State<MoviesTrailersPage> {
-  @override
+  Future<void> _createMovie(BuildContext context) async {
+    // await Navigator.of(context).push(
+    //   MaterialPageRoute(builder: (context) => const MoviesCreate()),
+    // );
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.movie.title)),
-      body: Column(),
+      appBar: AppBar(
+        title: const Text('Movies Index'),
+      ),
+      body: FutureBuilder<List<Trailer>>(
+        future: TrailerService().getAll(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return _MoviesTrailers(snapshot.data!, context);
+        },
+      ),
+    );
+  }
+
+  Widget _MoviesTrailers(List<Trailer> list, BuildContext context) {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Text(list[index].title),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+            ),
+          ),
+        );
+      },
     );
   }
 }
